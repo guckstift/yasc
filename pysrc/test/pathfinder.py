6a8bcc6ac7e1cbecbaa1e6adedfe7edd58294202
philipp <pthoericht@gmx.net>
2011-03-15 18:04:54 +0100

    improved pathfinding by access already computed pathes if start and end are the same

--- pysrc/test/pathfinder.py
+++ pysrc/test/pathfinder.py
@@ -3,174 +3,205 @@
 
 from path_heuristic import *
 from priorityqueueset import PriorityQueueSet
+from pathstorage import *
 
-class Node (object):
+class Pathfinder (object):
 	"""
-	Represents a single node at the map
+	The class organizing everything about pathfinding.
+	Needs following files:
+		path_heuristic.py
+		priorityqueueset.py
+		pathstorage.py
+		
+	TODO: 	*blocked nodes not implemented yet
+		*no error if coord is greater than border
+		*sometimes not the direct path is chosen, if another one has the 
+		same length	
 	"""
 
-	def __init__(self, coord, g = None, pred = None):
+	def __init__(self):
+		self.pathstorage = Pathstorage()
+
+	class Node (object):
 		"""
-		@param coord the coordinates of the node
-		@param g the cost from the startpoint to this point
-		@param pred a reference to the predecessor
+		Represents a single node at the map
 		"""
-		self.coord = coord	# the coordinates of this point
-		self.gcost = g		# the cost from the startpoint to this point
-		self.hcost = None	# the heuristic cost from this point to the endpoint
-		self.pred = pred	# the predecessor of this point at the path
+
+		def __init__(self, coord, g = None, pred = None):
+			"""
+			@param coord the coordinates of the node
+			@param g the cost from the startpoint to this point
+			@param pred a reference to the predecessor
+			"""
+			self.coord = coord	# the coordinates of this point
+			self.gcost = g		# the cost from the startpoint to this point
+			self.hcost = None	# the heuristic cost from this point to the endpoint
+			self.pred = pred	# the predecessor of this point at the path
 	
-	def __eq__(self, other):
-		return self.coord == other.coord
-        
-	def __cmp__(self, other):
-		return cmp(self.get_fcost(), other.get_fcost())
-        
-	def __hash__(self):
-		return hash(self.coord)
+		def __eq__(self, other):
+			return self.coord == other.coord
+		
+		def __cmp__(self, other):
+			return cmp(self.get_fcost(), other.get_fcost())
+		
+		def __hash__(self):
+			return hash(self.coord)
 				
-	def get_fcost(self):
-		"""
-		Returns the cost f(x) of this node
-		"""
-		return self.gcost + self.hcost
+		def get_fcost(self):
+			"""
+			Returns the cost f(x) of this node
+			"""
+			return self.gcost + self.hcost
 		
 
-def compute_path(start, end):
-	"""
-	Implements the A-Star algorithm (computes the path between start- and endpoint)
-	The path is returned as an iterator to the points, 
-	including the start- and endpoints themselves.
+	def compute_path(self, start, end):
+		"""
+		Implements the A-Star algorithm (computes the path between start- and endpoint)
+		The path is returned as an iterator to the points, 
+		including the start- and endpoints themselves.
+		@param start a 2-tuple with x- and y-coordinate of the startpoint
+		@param end same as start
+		@return if a path could be found it will be returned. If no path was found, 
+		an empty list is returned.
+		"""
+		# if the start- and endpoints are stored already, the stored path will be
+		# returned
+		path = self.pathstorage.search_path((start, end))
+		if path != []:
+			return path
 		
-	If no path was found, an empty list is returned.
+		# if the path isn't stored, it will be calculated:
+		sx = start[0]
+		sy = start[1]
+		ex = end[0]
+		ey = end[1]
+		closed_set = {}	# the set with the nodes along the path
+		open_set = PriorityQueueSet()	# nodes which are possible for the path
+		start_node = self.Node((sx, sy), 0, None)
+		start_node.hcost = path_heuristic((sx, sy), (ex, ey))
+		open_set.add(start_node)
 	
-	TODO: sometimes not the direct path is chosen, if another has the same length
-	"""
-	sx = start[0]
-	sy = start[1]
-	ex = end[0]
-	ey = end[1]
-	closed_set = {}	# the set with the nodes along the path
-	open_set = PriorityQueueSet()	# nodes which are possible for the path
-	start_node = Node((sx, sy), 0, None)
-	start_node.hcost = path_heuristic((sx, sy), (ex, ey))
-	open_set.add(start_node)
-	
-	while len(open_set) > 0:
-		curr_node = open_set.pop_smallest()
-            
-		if curr_node.coord == (ex, ey):
-			return _reconstruct_path(curr_node)
+		while len(open_set) > 0:
+			curr_node = open_set.pop_smallest()
+		    
+			if curr_node.coord == (ex, ey):
+				path = self._reconstruct_path(curr_node)
+				# make a list from the iterator "path" (more handy):
+				nodelist = []
+				for entry in path:
+					nodelist.append(entry)
+					
+				self.pathstorage.add(nodelist)
+				return nodelist
 		
-		closed_set[curr_node] = curr_node
+			closed_set[curr_node] = curr_node
 		
-		for succ_coord in _successors(curr_node.coord):
-			succ_node = Node(succ_coord)
-			succ_node.gcost = curr_node.gcost + 1	# the cost from one node to another is always 1
-			succ_node.hcost = path_heuristic(succ_node.coord, (ex, ey))
+			for succ_coord in self._successors(curr_node.coord):
+				succ_node = self.Node(succ_coord)
+				succ_node.gcost = curr_node.gcost + 1	# the cost from one node to another is always 1
+				succ_node.hcost = path_heuristic(succ_node.coord, (ex, ey))
 			
-			if succ_node in closed_set:
-				continue
+				if succ_node in closed_set:
+					continue
 				
-			if open_set.add(succ_node):
-				succ_node.pred = curr_node
+				if open_set.add(succ_node):
+					succ_node.pred = curr_node
 				
-	return []
+		return []
 
-def _successors(coord, border=8):
-	"""
-	Computes all successors of the given coord
-	@param coord the coordinate whoms successors will be computed
-	@param border x- and y-coordinate of the right and lower border
-	@return a List with all successing nodes
+	def _successors(self, coord, border=200):
+		"""
+		Computes all successors of the given coord
+		@param coord the coordinate whoms successors will be computed
+		@param border x- and y-coordinate of the right and lower border
+		@return a List with all successing nodes
+		"""
+		succ_list = []	# list with all possible successors of the given node
 	
-	TODO: blocked nodes not implemented yet
-		no error if coord is greater than border
-	"""
-	succ_list = []	# list with all possible successors of the given node
-	
-	if (coord[0] > 0 and coord[1] > 0) and (coord[0] < border and coord[1] < border):
-		if coord[1]%2 == 0:
-			succ_list.append((coord[0]-1, coord[1]-1))
-			succ_list.append((coord[0]-1, coord[1]+1))
-	
-		elif coord[1]%2 == 1:	# odd lineparity
-			succ_list.append((coord[0]+1, coord[1]-1))
-			succ_list.append((coord[0]+1, coord[1]+1))
-	
-		succ_list.append((coord[0]-1, coord[1]))
-		succ_list.append((coord[0], coord[1]-1))
-		succ_list.append((coord[0], coord[1]+1))
-		succ_list.append((coord[0]+1, coord[1]))
-	# behaviour at the borders and corners:
-	elif coord[0] == 0:	# left border
-		if coord[1] == 0:	# upper left corner
-			succ_list.append((0,1))
-			succ_list.append((1,0))
-		elif coord[1] == border:	# lower left corner
-			if coord[1]%2 == 1:
-				succ_list.append((coord[0]+1, coord[1]-1))
-			succ_list.append((coord[0], coord[1]-1))
-			succ_list.append((coord[0]+1, coord[1]))
-		else:	# somewhere between y == 0 and y == border
-			if coord[1]%2 == 1:
-				succ_list.append((coord[0]+1, coord[1]-1))
-				succ_list.append((coord[0]+1, coord[1]+1))
-			succ_list.append((coord[0], coord[1]-1))
-			succ_list.append((coord[0], coord[1]+1))
-			succ_list.append((coord[0]+1, coord[1]))
-	elif coord[1] == 0:		# upper border
-		if coord[0] == border:	# upper right corner
-			succ_list.append((coord[0]-1, 0))
-			succ_list.append((coord[0]-1, 1))
-			succ_list.append((coord[0], 1))
-		else:	# somewhere between x == 0 and x == border
-			succ_list.append((coord[0]-1, coord[1]+1))
-			succ_list.append((coord[0]-1, coord[1]))
-			succ_list.append((coord[0], coord[1]+1))
-			succ_list.append((coord[0]+1, coord[1]))
-	elif coord[0] == border:	# right border
-		if coord[1] == border:	# lower right corner
-			if coord[1]%2 == 0:
-				succ_list.append((coord[0]-1, coord[1]-1))
-			succ_list.append((coord[0]-1, coord[1]))
-			succ_list.append((coord[0], coord[1]-1))
-		else :	# somewhere between y == 0 and == border
+		if (coord[0] > 0 and coord[1] > 0) and (coord[0] < border and coord[1] < border):
 			if coord[1]%2 == 0:
 				succ_list.append((coord[0]-1, coord[1]-1))
 				succ_list.append((coord[0]-1, coord[1]+1))
+	
+			elif coord[1]%2 == 1:	# odd lineparity
+				succ_list.append((coord[0]+1, coord[1]-1))
+				succ_list.append((coord[0]+1, coord[1]+1))
+	
 			succ_list.append((coord[0]-1, coord[1]))
 			succ_list.append((coord[0], coord[1]-1))
 			succ_list.append((coord[0], coord[1]+1))
-	elif coord[1] == border:	# lower border
-		# somewhere between x == 0 and x == border
-		if coord[1]%2 == 0:
-			succ_list.append((coord[0]-1, coord[1]-1))
-		if coord[1]%2 == 1:
-			succ_list.append((coord[0]+1, coord[1]-1))
-		succ_list.append((coord[0]-1, coord[1]))
-		succ_list.append((coord[0], coord[1]-1))
-		succ_list.append((coord[0]+1, coord[1]))
+			succ_list.append((coord[0]+1, coord[1]))
+		# behaviour at the borders and corners:
+		elif coord[0] == 0:	# left border
+			if coord[1] == 0:	# upper left corner
+				succ_list.append((0,1))
+				succ_list.append((1,0))
+			elif coord[1] == border:	# lower left corner
+				if coord[1]%2 == 1:
+					succ_list.append((coord[0]+1, coord[1]-1))
+				succ_list.append((coord[0], coord[1]-1))
+				succ_list.append((coord[0]+1, coord[1]))
+			else:	# somewhere between y == 0 and y == border
+				if coord[1]%2 == 1:
+					succ_list.append((coord[0]+1, coord[1]-1))
+					succ_list.append((coord[0]+1, coord[1]+1))
+				succ_list.append((coord[0], coord[1]-1))
+				succ_list.append((coord[0], coord[1]+1))
+				succ_list.append((coord[0]+1, coord[1]))
+		elif coord[1] == 0:		# upper border
+			if coord[0] == border:	# upper right corner
+				succ_list.append((coord[0]-1, 0))
+				succ_list.append((coord[0]-1, 1))
+				succ_list.append((coord[0], 1))
+			else:	# somewhere between x == 0 and x == border
+				succ_list.append((coord[0]-1, coord[1]+1))
+				succ_list.append((coord[0]-1, coord[1]))
+				succ_list.append((coord[0], coord[1]+1))
+				succ_list.append((coord[0]+1, coord[1]))
+		elif coord[0] == border:	# right border
+			if coord[1] == border:	# lower right corner
+				if coord[1]%2 == 0:
+					succ_list.append((coord[0]-1, coord[1]-1))
+				succ_list.append((coord[0]-1, coord[1]))
+				succ_list.append((coord[0], coord[1]-1))
+			else :	# somewhere between y == 0 and == border
+				if coord[1]%2 == 0:
+					succ_list.append((coord[0]-1, coord[1]-1))
+					succ_list.append((coord[0]-1, coord[1]+1))
+				succ_list.append((coord[0]-1, coord[1]))
+				succ_list.append((coord[0], coord[1]-1))
+				succ_list.append((coord[0], coord[1]+1))
+		elif coord[1] == border:	# lower border
+			# somewhere between x == 0 and x == border
+			if coord[1]%2 == 0:
+				succ_list.append((coord[0]-1, coord[1]-1))
+			if coord[1]%2 == 1:
+				succ_list.append((coord[0]+1, coord[1]-1))
+			succ_list.append((coord[0]-1, coord[1]))
+			succ_list.append((coord[0], coord[1]-1))
+			succ_list.append((coord[0]+1, coord[1]))
 		
-	return succ_list
+		return succ_list
 
-def _reconstruct_path(node):
-	""" 
-	Reconstructs the path to the given node from the start node
-	(for which .pred is None)
-	"""
-	path = [node.coord]
-	n = node
-	while n.pred:
-		n = n.pred
-		path.append(n.coord)
-        return reversed(path)
+	def _reconstruct_path(self, node):
+		""" 
+		Reconstructs the path to the given node from the start node
+		(for which .pred is None)
+		@param node the endnode at the path
+		@return an iterator representing the path
+		"""
+		path = [node.coord]
+		n = node
+		while n.pred:
+			n = n.pred
+			path.append(n.coord)
+		return reversed(path)
 	
-def a_star(start, end):
-	"""
-	For simple testing at the console, not for productive use
-	"""
-	path = compute_path(start, end)
-	for node in path:
-		print node
+	def a_star(self, start, end):
+		"""
+		For simple testing at the console, for productive use take compute_path()
+		"""
+		path = self.compute_path(start, end)
+		for node in path:
+			print node
 
---
+++ pysrc/test/pathstorage.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+class Pathstorage (object):
+
+	def __init__(self):
+		self.storage ={}	# the storage itself
+		self.entries = 0	# the number of entries in the storage
+
+	def add(self, path, replace=True):
+		"""
+		Adds a new path into the storage. Normally a path with the same key, but another
+		value will overwrite an old one. Control this behaviour with the replace-switch.
+		@param path the path to be stored (must be a non empty list of 2-tuples)
+		@param replace by default this switch is true, which means newer pathes will
+			replace older ones with the same key (start- and endpoint)
+		"""		
+		self.length1 = len(path) - 1
+		
+		if replace == True:
+			self.storage[(path[0], path[self.length1])] = path
+		else:	# otherwise the storage must be checked if there is the same key
+			newkey = (path[0], path[self.length1])
+			if newkey in self.storage.keys():
+				pass	# if the newkey is already in the storage
+			else:
+				self.storage[newkey] = path
+				
+	def delete(self, path=None, pathkey=None):
+		"""
+		Removes a single path from the storage. If nothing is passed to this method,
+		nothing will be deleted.
+		@param path a hole path to be deleted
+		@param pathkey the start- and endpoint of the path (must be a 2-tuple of 
+			2-tuples) to be deleted
+		"""
+		if path == None and pathkey == None:
+			pass
+		elif pathkey != None:
+			del self.storage[pathkey]
+		else:
+			self.length2 = len(path) - 1
+			for key in self.storage:
+				if key == (path[0], path[self.length2]):
+					del self.storage[key]
+					break
+	
+	def search_path(self, pathkey):
+		"""
+		Searches a path in the storage by its key, which is a tuple with the 
+		start- and the endpoint of the path.
+		@param pathkey a 2-tuple of 2-tuples 
+		@return returns either the path if it is in the storage or an empty list if not
+		"""
+		if pathkey in self.storage.keys():
+			return self.storage[pathkey]
+		else:
+			return []
